a1b6185f3577da0ae5f073904ee96a5d7e77fbe2
admin <chengpf_0927@126.com>
2015-04-30 16:04:12 +0800

    import-summary

---
+++ app/src/main/java/com/qianfeng/chanyouji/urls/Urls.java
@@ -0,0 +1,12 @@
+package com.qianfeng.chanyouji.urls;
+
+/**
+ * Created by admin on 2015/4/30.
+ */
+public class Urls {
+
+    //专题的接口  page=1
+    public static String Subject_ShouYe="https://chanyouji.com/api/articles.json?page=1";
+
+
+}
