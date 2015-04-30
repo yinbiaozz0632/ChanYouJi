e143836a923a9bd73e84c37bebe0f60bfa54c621
yichengzou <857659251@qq.com>
2015-04-30 17:59:49 +0800

    json 解析加载

--- app/src/main/java/com/qianfeng/chanyouji/netutils/PaseJson.java
+++ app/src/main/java/com/qianfeng/chanyouji/netutils/PaseJson.java
@@ -1,20 +1,49 @@
 package com.qianfeng.chanyouji.netutils;
 
+import com.qianfeng.chanyouji.beans.Destination;
+import com.qianfeng.chanyouji.beans.DestinationsDatas;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by aaa on 15-4-30.
  */
 public class PaseJson {
 
-    public static void jsonToList(String json){
+    public static List<DestinationsDatas> jsonToList(String json){
         try {
             JSONArray array = new JSONArray(json);
+            List<DestinationsDatas> list=new ArrayList<>();
+            for (int i = 0; i <array.length(); i++) {
+                DestinationsDatas destinationsDatas = new DestinationsDatas();
+                JSONObject jsonObject = array.getJSONObject(i);
+                JSONArray destinations = jsonObject.getJSONArray("destinations");
+                List<Destination> list1=new ArrayList<>();
+                for (int j = 0; j < destinations.length(); j++) {
+                    Destination destination = new Destination();
+                    JSONObject jsonObject1 = destinations.getJSONObject(j);
+                    destination.setImage_Url(jsonObject1.getString("image_url"));
+                    destination.setName_En(jsonObject1.getString("name_en"));
+                    destination.setName_Zh_Cn(jsonObject1.getString("name_zh_cn"));
+                    destination.setPoi_Count(jsonObject1.getString("poi_count"));
+                    list1.add(destination);
+                }
+                destinationsDatas.setDestinations(list1);
+                list.add(destinationsDatas);
+                return list;
+            }
+            System.out.println(list.toString());
+
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
 }
