09e5e538b68cd21fe0a2d43e6e1c443bf5d32752
admin <chengpf_0927@126.com>
2015-04-30 16:10:23 +0800

    增加了Volley和xUtils

--- app/src/main/java/com/qianfeng/chanyouji/netutils/BitmapHelper.java
+++ app/src/main/java/com/qianfeng/chanyouji/netutils/BitmapHelper.java
@@ -1,6 +1,7 @@
 package com.qianfeng.chanyouji.netutils;
 
 import android.content.Context;
+import android.os.Environment;
 
 import com.lidroid.xutils.BitmapUtils;
 
@@ -11,8 +12,7 @@ public class BitmapHelper {
     private static BitmapUtils bitmapUtils;
 
     public static void initBitmapUtils(Context context){
-
-        bitmapUtils=new BitmapUtils(context);
+        bitmapUtils=new BitmapUtils(context, Environment.getDownloadCacheDirectory()+"/ChouYouJi/images",1/8.0f,10*1024*1024);
     }
 
     public static BitmapUtils getBitmapUtils(){
