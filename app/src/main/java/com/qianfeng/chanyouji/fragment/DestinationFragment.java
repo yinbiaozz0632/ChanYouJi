1031416bb0f082b0e6fbab007113c77e19d8c8e7
yichengzou <857659251@qq.com>
2015-05-01 15:16:27 +0800

    kk

--- app/src/main/java/com/qianfeng/chanyouji/fragment/DestinationFragment.java
+++ app/src/main/java/com/qianfeng/chanyouji/fragment/DestinationFragment.java
@@ -5,31 +5,45 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.qianfeng.chanyouji.R;
-import com.qianfeng.chanyouji.finalutils.FinalUrl;
+import com.qianfeng.chanyouji.adapter.DestinationsAdapter;
+import com.qianfeng.chanyouji.beans.DestinationsDatas;
 import com.qianfeng.chanyouji.netutils.DownLoadData;
+import com.qianfeng.chanyouji.netutils.PaseJson;
+import com.qianfeng.chanyouji.urls.FinalUrl;
+
+import java.util.List;
 
 /**
  * Created by admin on 2015/4/30.
  */
 public class DestinationFragment extends Fragment implements PullToRefreshBase.OnRefreshListener<ListView> {
     private PullToRefreshListView pullToRefalsh;
+    private List<DestinationsDatas> destinationsDatases;
+    private DestinationsAdapter adapter;
     private Handler handler=new Handler(){
+
         @Override
         public void handleMessage(Message msg) {
             if (msg.what==1) {
                 String s = (String) msg.obj;
                 //解析数据
-
-
+               // destinationsDatases = PaseJson.jsonToList(s);
+               // destinationsDatases = PaseJson.jsonToList(s);
+                destinationsDatases = PaseJson.jsonToList(s);
+                adapter = new DestinationsAdapter(getActivity(),destinationsDatases);
+                pullToRefalsh.setAdapter(adapter);
 
             }
         }
@@ -37,22 +51,22 @@ public class DestinationFragment extends Fragment implements PullToRefreshBase.O
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ImageView img=new ImageView(getActivity());
         View view = inflater.inflate(R.layout.destination, container, false);
+
         pullToRefalsh = ((PullToRefreshListView) view.findViewById(R.id.pulltolistview));
         DownLoadData.downData(getActivity(), FinalUrl.DESTINATION,handler,1);
         //下拉监听
         pullToRefalsh.setOnRefreshListener(this);
-
+        //pullToRefalsh.getRefreshableView(); 将pulltorefalsh转成普通的listView可以添加头部
 
         return view;
     }
 
     @Override
     public void onRefresh(PullToRefreshBase<ListView> refreshView) {
-
-
+        DownLoadData.downData(getActivity(), FinalUrl.DESTINATION,handler,1);
 
         pullToRefalsh.onRefreshComplete();
     }
+
 }
